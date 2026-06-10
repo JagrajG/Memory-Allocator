55cb4d23bd21a99291d304763ddcb7d67fe55c91
JagrajG <jagrajgill83@gmail.com>
2026-06-10 10:01:26 -0700

    added heap_dump function

--- main.c
+++ main.c
@@ -4,12 +4,6 @@
 
 #define HEAP_SIZE 4096
 
-uint8_t heap[HEAP_SIZE];
-
-// keep track of how many of the HEAP_Size is used so we do not exceed it
-size_t heap_used = 0;
-
-struct Header *head = NULL;
 struct Header
 {
     // size of user memory, not including header
@@ -21,6 +15,13 @@ struct Header
     // pointer to next block
     struct Header *next;
 };
+
+struct Header *head = NULL;
+
+uint8_t heap[HEAP_SIZE];
+
+// keep track of how many of the HEAP_Size is used so we do not exceed it
+size_t heap_used = 0;
 
 void *my_malloc(size_t size)
 {
@@ -35,8 +36,8 @@ void *my_malloc(size_t size)
     {
         return NULL;
     }
-    // create the meta data
 
+    // create the meta data
     // create a pointer to a Header called header
     // header takes the shape of Header
     // heap + heap_used is used to find the next open spot in memory
@@ -45,7 +46,7 @@ void *my_malloc(size_t size)
     header->is_free = 0;
     header->next = NULL;
 
-    // link to previous block
+    // make head point to the head of the LL
     if (head == NULL)
     {
         head = header;
@@ -65,6 +66,37 @@ void *my_malloc(size_t size)
     return (void *)(header + 1);
 }
 
+void my_free(void *ptr)
+{
+    if (ptr == NULL)
+    {
+        return;
+    }
+
+    struct Header *header = ((struct Header *)ptr) - 1;
+    header->is_free = 1;
+}
+
+void heap_dump(void)
+{
+    struct Header *header = head;
+    int block = 0;
+
+    while (header != NULL)
+    {
+        printf("Block: %d | size: %zu | is_free: %u | next: %p\n",
+               block,
+               header->size,
+               header->is_free,
+               (void *)header->next);
+
+        header = header->next;
+        block++;
+    }
+
+    printf("heap_used: %zu / %d\n", heap_used, HEAP_SIZE);
+}
+
 int main(void)
 {
     void *a = my_malloc(100);
@@ -77,5 +109,7 @@ int main(void)
     printf("c = %p\n", c);
     printf("d = %p\n", d);
 
-    return 0;
+    my_free(a);
+
+    heap_dump();
 }
